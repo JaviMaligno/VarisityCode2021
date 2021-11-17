a4d89717754a0474344fd627f92bf91475906627
Javi <javiecija96@gmail.com>
2021-11-17 23:10:47 +0000

    fixed errors

---
+++ Challenge 2 - ARM V1/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 20:46:05 2021
+
+@author: javia
+"""
+
+class Solution:
+
+    def number_prediction(self, input):
+        # Your code goes here
+        pass
+    
